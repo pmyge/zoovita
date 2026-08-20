4c6cafa4345006e9076316dedad923757c39ecfb
ergashboy <ergashmasharipov88@gmail.com>
2026-08-20 22:01:24 +0500

    Optimize CI/CD to build Node apps on GitHub runners instead of production server

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,8 @@ cd /root/zoovita
 git fetch --all
 git reset --hard origin/main
 
+# Backend'ni qayta ishga tushirish
 systemctl restart zoovita-api
 
-cd /root/zoovita/admin-panel
-npm install
-npm run build
-rm -rf /var/www/zoovita/*
-cp -r dist/* /var/www/zoovita/
-
-cd /root/zoovita/web_site
-npm install
-npm run build
-rm -rf /var/www/zoovita-site/*
-cp -r dist/* /var/www/zoovita-site/
-
+# Nginx'ni qayta yuklash
 systemctl reload nginx
