42f09d13a37eb67a35c144bd317df6b510389e58
ergashboy <ergashmasharipov88@gmail.com>
2026-08-22 21:52:24 +0500

    Revert to local building now that server has 2GB RAM

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# Loyiha papkasiga o'tish
 cd /root/zoovita
 
+# Oxirgi kodni yuklab olish
 git fetch --all
 git reset --hard origin/main
 
-# Backend'ni qayta ishga tushirish
+# Backend'ni qayta yuklash
 systemctl restart zoovita-api
+
+# Admin panelni yig'ish (Server endi 2GB RAM bo'lgani uchun o'zida yig'adi)
+cd /root/zoovita/admin-panel
+npm install
+npm run build
+rm -rf /var/www/zoovita/*
+cp -r dist/* /var/www/zoovita/
+
+# Veb-saytni yig'ish
+cd /root/zoovita/web_site
+npm install
+npm run build
+rm -rf /var/www/zoovita-site/*
+cp -r dist/* /var/www/zoovita-site/
 
 # Nginx'ni qayta yuklash
 systemctl reload nginx
